186437a795e821b3f7ce57c0e74b3a136800e3db
nsangana <125701360+nsangana@users.noreply.github.com>
2023-05-12 14:38:51 -0700

    Delete xomics directory

--- xomics/single-cell-rna/Mock Data Test Scala.scala
+++
@@ -1,9 +0,0 @@
-// Databricks notebook source
-sql("USE CATALOG hive_metastore")
-sql("USE SCHEMA srijit_nair")
-
-val mm = sql("select * from matrix").collect()
-
-// COMMAND ----------
-
-
--- xomics/single-cell-rna/R Feather Read.r
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-mm_f <- arrow::read_feather("/dbfs/dbfs/srijit.nair/temp/feather")
-
-# COMMAND ----------
-
-
